d6c8aa3f41f1dea14024daded7c246dfc65d9ab4
Pablo Andrés Dorado Suárez <im@pandres95.me>
2016-10-31 14:18:35 -0500

    Fix– Gulp Tasks.

--- gulpfile.js
+++ gulpfile.js
@@ -4,8 +4,4 @@
 var sequence    = require('gulp-sequence')
 ,   gulp        = require('gulp-folder-load')('tasks');
 
-gulp.task('prepare', sequence(['template', 'symlink'], 'wiredep', 'inject'));
-gulp.task('default', sequence('prepare', 'serve'));
-gulp.task(
-    'build', sequence('prepare', ['assets', 'fonts'], 'source', 'components')
-);
+gulp.task('default', sequence('prepare', ':serve'));
